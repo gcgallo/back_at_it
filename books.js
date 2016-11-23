2a939d483cf4596f8f8bcd2c945f854f61e33237
gcgallo <ggallo102@gmail.com>
2016-11-23 00:37:15 -0800

    Update books.js

--- books.js
+++ books.js
@@ -8,7 +8,7 @@ var easing = .3;
     
 function setup(){
     
-	createCanvas(720, 600);
+	createCanvas(725, 480);
 	img.push(loadImage("download7.png"));
 	img.push(loadImage("download6.png"));
 	img.push(loadImage("download5.png"));
@@ -17,95 +17,64 @@ function setup(){
 	img.push(loadImage("download2.png"));
 	img.push(loadImage("download1.png"));
 	img.push(loadImage("download0.png"));
-	w = 730;
+	w = 725;
 	h = 480;
 }
 
 
 function draw(){
     
+	tint(255, 255);
+	
 	if (x < 255){
-		
-		tint(255, 255 - x);
+	
 		
 		image(img[0], 0, 0, w, h);
-		
-		tint(255, x);
-		
-		image(img[1], 0, 0, w, h);
-		
+	
 		
 	} 
 	else if(x < 512){
-		
-		tint(255, 512 - x);
-		
+				
 		image(img[1], 0, 0, w, h);
-		
-		tint(255, x - 255);
-		
-		image(img[2], 0, 0, w, h);
-		 
+				 
 		
 	}
 	else if(x < 767){
-		
-		tint(255, 767 - x);
+
 		
 		image(img[2], 0, 0, w, h);
-		
-		tint(255, x - 512);
-		
-		image(img[3], 0, 0, w, h);
+
 		 
 		
 	}
 	else if(x < 1024){
 		
-		tint(255, 1024 - x);
+
 		
 		image(img[3], 0, 0, w, h);
 		
-		tint(255, x - 767);
-		
-		image(img[4], 0, 0, w, h);
-		 
-		
+
 	}
 	else if(x < 1279){
 		
-		tint(255, 1279 - x);
-		
+
 		image(img[4], 0, 0, w, h);
-		
-		tint(255, x - 1024);
-		
-		image(img[5], 0, 0, w, h);
-		 
+
 		
 	}
 	else if(x < 1534){
 		
-		tint(255, 1534 - x);
 		
 		image(img[5], 0, 0, w, h);
 		
-		tint(255, x - 1279);
-		
-		image(img[6], 0, 0, w, h);
 		
 		
 	}
 	
 	else if(x < 1788){
-		
-		tint(255, 1788 - x);
-		
+				
 		image(img[6], 0, 0, w, h);
-		
-		tint(255, x - 1534);
-		
-		image(img[7], 0, 0, w, h);
+
 		
 		
 	}
